9f4085168e59a5fb826146a5b9e8e60b7e4f1259
Radhakrishhnan Ganapathy <85297693+radhakrishnanganapathy@users.noreply.github.com>
2024-01-25 12:34:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ col0,col1 = st.columns(2)
 col0.write("Enter a APIKEY, ChannelIDS and page/data limit to get channel data and click here -->")
 if col1.button("chanel"):
     channel_return = youtube_Channel_analysis(youtube,api_key,channel_id)
-if st.button((video"):
+if st.button("video"):
     video_return = get_video_info(youtube,api_key,channel_id,resultLimit,pageLimit)
 if st.button("playlist"):
     playlist_return = get_playlist_info(youtube,api_key,channel_id,resultLimit,pageLimit)
